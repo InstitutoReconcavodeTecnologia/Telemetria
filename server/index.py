96e837f47600ec0c6f2cec705117206df9eca424
Antonio Fernando Santos Ladeia <contato@antonioladeia.com>
2017-10-17 14:05:01 -0300

    verify post request

--- server/index.py
+++ server/index.py
@@ -8,11 +8,10 @@ app = Flask(__name__)
 @app.route("/insertData", methods=['GET', 'POST'])
 def insert_data():
     print(request)
-    data = request.get_json(force=True)
-
-    
-    print(data[u'temp'])
-    print(data[u'hum'])
+    if request.method == POST:
+        data = request.get_json(force=True)
+        print(data[u'temp'])
+        print(data[u'hum'])
 
     return "insert data"
 
