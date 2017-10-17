8cd7aa88f6b4744a4ec5c631c318306a81555e98
grupo de embarcados <embarcados@reconcavo.org.br>
2017-10-17 13:56:03 -0300

    add simple flask server funcionality

---
+++ server/index.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask import request
+import json
+
+app = Flask(__name__)
+
+
+@app.route("/insertData", methods=['GET', 'POST'])
+def insert_data():
+    print(request)
+    data = request.get_json(force=True)
+
+    
+    print(data[u'temp'])
+    print(data[u'hum'])
+
+    return "insert data"
+
+
+@app.route("/")
+def hello():
+    return "test flask"
+
