25b5367022130e7f6716881b691ceae2eeaaf422
grupo de embarcados <embarcados@reconcavo.org.br>
2017-10-20 13:51:24 -0300

    Added feature data insertion on database

--- server/index.py
+++ server/index.py
@@ -26,14 +26,29 @@ def init_db():
             db.cursor().executescript(f.read())
         db.commit()
 
+def insert_db(sensor, value):
+	print(type(sensor))
+	print(type(value))
+	db = get_db()
+	query = 'INSERT INTO measurement (sensor, value)'\
+	' VALUES ("'+ sensor +'", '+ str(value) +')'
+
+	print(query)
+	db.cursor().execute(query)
+	db.commit()
+	id_inserted =  db.cursor().lastrowid
+	db.cursor().close()
+	return id_inserted
+
 
 @app.route("/insertData", methods=['GET','POST'])
 def insert_data():
     data = request.get_json(force=True)
     print(data[u'temp'])
     print(data[u'hum'])
-
-    return "insert data"
+    tmp1 = insert_db('temp', data[u'temp'])
+    tmp2 = insert_db('hum', data[u'hum'])
+    return "insert data" + str(tmp1) + str(tmp2)
 
 
 @app.route("/")
