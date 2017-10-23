60dbd6e380e307c150461a8f52761bea2cb3a30c
grupo de embarcados <embarcados@reconcavo.org.br>
2017-10-23 14:08:48 -0300

    Added feature to retrieve data.

--- server/index.py
+++ server/index.py
@@ -1,6 +1,5 @@
-import json
 import sqlite3
-from flask import Flask, request, g
+from flask import Flask, request, g, jsonify
 
 app = Flask(__name__)
 
@@ -13,11 +12,13 @@ def get_db():
         db = g._database = sqlite3.connect(DATABASE)
     return db
 
+
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, 'sensor_data', None)
     if db is not None:
         db.close()
+
 
 def init_db():
     with app.app_context():
@@ -26,33 +27,57 @@ def init_db():
             db.cursor().executescript(f.read())
         db.commit()
 
+
 def insert_db(sensor, value):
-	print(type(sensor))
-	print(type(value))
-	db = get_db()
-	query = 'INSERT INTO measurement (sensor, value)'\
-	' VALUES ("'+ sensor +'", '+ str(value) +')'
-
-	print(query)
-	db.cursor().execute(query)
-	db.commit()
-	id_inserted =  db.cursor().lastrowid
-	db.cursor().close()
-	return id_inserted
+    db = get_db()
+    query = 'INSERT INTO measurement (sensor, value)'\
+        ' VALUES ("' + sensor + '", ' + str(value) + ')'
+    db.cursor().execute(query)
+    db.commit()
+    id_inserted = db.cursor().lastrowid
+    db.cursor().close()
+    return id_inserted
 
 
-@app.route("/insertData", methods=['GET','POST'])
+def select_all_db():
+    db = get_db()
+    query = 'SELECT * FROM measurement;'
+    cur = db.cursor().execute(query)
+    result = cur.fetchall()
+    cur.close()
+    return result
+
+
+def serialize(result):
+    d = []
+    for id, sensor, value, timestamp in result:
+        m = {}
+        m["id"] = id
+        m["sensor"] = sensor
+        m["value"] = value
+        m["timestamp"] = timestamp
+        d.append(m)
+
+    return d
+
+
+@app.route("/insertData", methods=['GET', 'POST'])
 def insert_data():
     data = request.get_json(force=True)
-    print(data[u'temp'])
-    print(data[u'hum'])
-    tmp1 = insert_db('temp', data[u'temp'])
-    tmp2 = insert_db('hum', data[u'hum'])
-    return "insert data" + str(tmp1) + str(tmp2)
+    insert_db('temp', data[u'temp'])
+    insert_db('hum', data[u'hum'])
+    return "Data inserted sucessfully!"
+
+
+@app.route("/getData", methods=['GET', 'POST'])
+def get_data():
+    result = select_all_db()
+    d = serialize(result)
+
+    return jsonify(d)
 
 
 @app.route("/")
 def hello():
     init_db()
-    cur = get_db().cursor()
-    return "test flask"
+    return "Server is Running..."
